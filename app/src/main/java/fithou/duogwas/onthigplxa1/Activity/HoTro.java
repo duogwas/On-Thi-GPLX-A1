780b6423bee50a16998d1ff5347d98878b49e638
duogwas <duong2001n@gmail.com>
2021-11-12 21:53:34 +0700

    xong layout ThongTinUngDung

--- app/src/main/java/fithou/duogwas/onthigplxa1/Activity/HoTro.java
+++ app/src/main/java/fithou/duogwas/onthigplxa1/Activity/HoTro.java
@@ -3,12 +3,16 @@ package fithou.duogwas.onthigplxa1.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import fithou.duogwas.onthigplxa1.R;
 
 public class HoTro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_ho_tro);
     }
 }
--- app/src/main/java/fithou/duogwas/onthigplxa1/Activity/ThongTinUngDung.java
+++ app/src/main/java/fithou/duogwas/onthigplxa1/Activity/ThongTinUngDung.java
@@ -2,15 +2,29 @@ package fithou.duogwas.onthigplxa1.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
 import fithou.duogwas.onthigplxa1.R;
 
 public class ThongTinUngDung extends AppCompatActivity {
+    ImageButton img_BtnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_thong_tin_ung_dung);
+        img_BtnHome=findViewById(R.id.img_btnHome);
+        img_BtnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ThongTinUngDung.this,MainActivity.class));
+            }
+        });
+
     }
 }

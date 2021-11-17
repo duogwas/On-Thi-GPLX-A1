f8dceaf701d521a60f85d7249ee0896cd649c5b9
duogwas <duong2001n@gmail.com>
2021-11-17 11:05:28 +0700

    custom list view bien bao

---
+++ app/src/main/java/fithou/duogwas/onthigplxa1/Adapter/Custom_lv_BienBao.java
@@ -0,0 +1,16 @@
+package fithou.duogwas.onthigplxa1.Adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fithou.duogwas.onthigplxa1.R;
+
+public class Custom_lv_BienBao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_lv_bien_bao);
+    }
+}
